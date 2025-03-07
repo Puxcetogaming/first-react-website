bc00d7b11e5e8fbdac26a2f926bc363862de6e79
Puxcetogaming <yani.slavchov@gmail.com>
2025-03-07 19:30:10 +0000

    Removing everything to see if we can get a clean slate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h3>My First React Edit</h3>
-        <h1>This will be dedicated to learning how to do front-end development with React</h1>
-      </header>
+    <div>
+      <h1>Welcome to Your Bare Bones React App</h1>
     </div>
   );
 }
